70a91c136291ea68bbafd337ecf65ee351f78325
Murilo <murilinhops@hotmail.com>
2020-04-07 00:00:12 -0300

    changed variables

--- src/controllers/servicesControllers.js
+++ src/controllers/servicesControllers.js
@@ -1,9 +1,9 @@
 const sgMail = require('@sendgrid/mail');
-const client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
+const client = require('twilio')('ACcab532724545f1d87d44ffb4ee01d52f' '9f3813e03072586e0d6210f7141d0aec');
 
 const sendEmail = async (req, res) => {
   try {
-    sgMail.setApiKey(process.env.MAIL_KEY);
+    sgMail.setApiKey('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');
     const resolved = await sgMail.send(req.body);
 
     return res.status(200).json(resolved);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server listeinig on port: ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listeinig on port: ${process.env.PORT}`);
 });
