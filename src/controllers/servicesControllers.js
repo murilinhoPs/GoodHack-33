dd015dfe6177de7ad1d7935627fe8a9e8c7f5b30
Murilo <murilinhops@hotmail.com>
2020-04-07 00:01:39 -0300

    VIRGULA

--- src/controllers/servicesControllers.js
+++ src/controllers/servicesControllers.js
@@ -1,5 +1,5 @@
 const sgMail = require('@sendgrid/mail');
-const client = require('twilio')('ACcab532724545f1d87d44ffb4ee01d52f' '9f3813e03072586e0d6210f7141d0aec');
+const client = require('twilio')('ACcab532724545f1d87d44ffb4ee01d52f', '9f3813e03072586e0d6210f7141d0aec');
 
 const sendEmail = async (req, res) => {
   try {
